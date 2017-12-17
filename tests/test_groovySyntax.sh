7cefdb89c89fc82f2489d5bdeea20922cf51a726
Olivier Clavel <olivier.clavel@gmail.com>
2017-12-17 19:38:16 +0100

    Added a new groovy template for backups and its syntax check

--- tests/test_groovySyntax.sh
+++ tests/test_groovySyntax.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 thisScriptPath=`dirname $(readlink -f $0)`
-groovyScriptsPath=`dirname $thisScriptPath`/files/groovy
+rolePath=`dirname $thisScriptPath`
+groovyScriptsPath=${rolePath}/files/groovy
 groovyChecker=$thisScriptPath/syntaxChecking.groovy
 exitStatus=0
 
-echo Checking scripts in files/groovy
+echo Checking scripts in files/groovy and template templates/backup.groovy.j2
+scriptFiles=`ls -d -A -1 $groovyScriptsPath/*`
+scriptFiles="${scriptFiles} ${rolePath}/templates/backup.groovy.j2"
 
-for script in `ls $groovyScriptsPath`; do
+for script in ${scriptFiles} ; do
     echo Testing groovy syntax for $script
     groovy $groovyChecker $script
     if [ ! $? -eq 0 ]; then
