ca103edc3d3c020497822d2ff489f2a9ae189021
Olivier Clavel <olivier.clavel@gmail.com>
2018-02-10 19:40:51 +0100

    Add npm repos and test to download scoped packages

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_npm_scoped_package_download(host):
+    test_package_url = \
+        "https://localhost/repository/npm-public/@angular%2fcore"
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    get_url_options = \
+        "url='%s' dest='/tmp/testfile' force='yes' validate_certs='no'" \
+        % (test_package_url)
+
+    download = host.ansible(
+               "get_url",
+               get_url_options,
+               check=False
+               )
+
+    assert download["status_code"] == 200
+    assert download.state == file
