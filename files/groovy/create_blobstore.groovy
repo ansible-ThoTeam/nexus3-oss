fd48134ce472ccbfb7ee4297010283eebc9241c6
Valentin Gurmeza <valentin.gurmeza@globaleagle.com>
2018-07-26 17:00:43 -0700

    add s3 blobstore implementation

--- files/groovy/create_blobstore.groovy
+++ files/groovy/create_blobstore.groovy
@@ -4,5 +4,9 @@ parsed_args = new JsonSlurper().parseText(args)
 
 existingBlobStore = blobStore.getBlobStoreManager().get(parsed_args.name)
 if (existingBlobStore == null) {
-    blobStore.createFileBlobStore(parsed_args.name, parsed_args.path)
+  if (parsed_args.type == "S3") {
+      blobStore.createS3BlobStore(parsed_args.name, parsed_args.config)
+  } else {
+      blobStore.createFileBlobStore(parsed_args.name, parsed_args.path)
+  }
 }
