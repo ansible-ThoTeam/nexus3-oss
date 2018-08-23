957e9a1bd533468d2978d970c493d8b0b1648c67
Rodrigo Núñez <rodrigo.nunez@devo.com>
2018-08-23 16:51:11 +0200

    Added necessary changes for Nexus ansible playbook

---
+++ files/groovy/create_repo_apt_hosted.groovy
@@ -0,0 +1,45 @@
+import groovy.json.JsonSlurper
+import org.sonatype.nexus.repository.config.Configuration
+
+parsed_args = new JsonSlurper().parseText(args)
+
+repositoryManager = repository.repositoryManager
+
+existingRepository = repositoryManager.get(parsed_args.name)
+
+if (existingRepository != null) {
+
+    newConfig = existingRepository.configuration.copy()
+    // We only update values we are allowed to change (cf. greyed out options in gui)
+    newConfig.attributes['apt']['distribution'] = parsed_args.distribution
+    newConfig.attributes['aptSigning']['keypair'] = parsed_args.keypair
+    newConfig.attributes['aptSigning']['passphrase'] = parsed_args.passphrase
+    newConfig.attributes['storage']['writePolicy'] = parsed_args.write_policy.toUpperCase()
+    newConfig.attributes['storage']['strictContentTypeValidation'] = Boolean.valueOf(parsed_args.strict_content_validation)
+
+    repositoryManager.update(newConfig)
+
+} else {
+
+    configuration = new Configuration(
+        repositoryName: parsed_args.name,
+        recipeName: 'apt-hosted',
+        online: true,
+        attributes: [
+            apt  : [
+                distribution : parsed_args.distribution
+            ],
+            aptSigning: [
+                keypair: parsed_args.keypair,
+                passphrase: parsed_args.passphrase        
+            ],
+            storage: [
+                writePolicy: parsed_args.write_policy.toUpperCase(),
+                blobStoreName: parsed_args.blob_store,
+                strictContentTypeValidation: Boolean.valueOf(parsed_args.strict_content_validation)
+            ]
+        ]
+    )
+
+    repositoryManager.create(configuration)
+}
