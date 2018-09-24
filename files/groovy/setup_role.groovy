7d49a24d5d6e64e036bd2bfcd5bcbf3e7cd31e13
Olivier Clavel <olivier.clavel@gmail.com>
2018-09-24 16:40:29 +0200

    Rewrite + add some logging for setup_role

--- files/groovy/setup_role.groovy
+++ files/groovy/setup_role.groovy
@@ -6,23 +6,18 @@ parsed_args = new JsonSlurper().parseText(args)
 
 authManager = security.getSecuritySystem().getAuthorizationManager(UserManager.DEFAULT_SOURCE)
 
-def existingRole = null
-
-try {
-    existingRole = authManager.getRole(parsed_args.id)
-} catch (NoSuchRoleException ignored) {
-    // could not find role
-}
-
 privileges = (parsed_args.privileges == null ? new HashSet() : parsed_args.privileges.toSet())
 roles = (parsed_args.roles == null ? new HashSet() : parsed_args.roles.toSet())
 
-if (existingRole != null) {
+try {
+    existingRole = authManager.getRole(parsed_args.id)
     existingRole.setName(parsed_args.name)
     existingRole.setDescription(parsed_args.description)
     existingRole.setPrivileges(privileges)
     existingRole.setRoles(roles)
     authManager.updateRole(existingRole)
-} else {
+    log.info("Role {} updated", parsed_args.name)
+} catch (NoSuchRoleException ignored) {
     security.addRole(parsed_args.id, parsed_args.name, parsed_args.description, privileges.toList(), roles.toList())
+    log.info("Role {} created", parsed_args.name)
 }
