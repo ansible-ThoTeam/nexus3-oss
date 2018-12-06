ac03d1d5c247d5f54e3262c3c298211818c405f0
Charles DELFLY <c.delfly@linkbynet.com>
2018-12-06 13:58:48 +0700

    Check if repo name exist and delete and destroy the repository

--- files/groovy/delete_repo.groovy
+++ files/groovy/delete_repo.groovy
@@ -1,5 +1,31 @@
+/**
+ * Delete the repsotiroy of the name given in input if exist
+ *
+ * Input is json:
+ *   name: Repository name
+ * Output is json:
+ *   result: "true" if ok
+ *           "error message" if not ok
+ *           "null" if repository doesn't exist
+ *
+ **/
+
 import groovy.json.JsonSlurper
+import org.sonatype.nexus.repository.Repository
 
-parsed_args = new JsonSlurper().parseText(args)
+def arg = new JsonSlurper().parseText(args)
 
-repository.getRepositoryManager().delete(parsed_args.name)
+if(arg.name == null) {
+    return "you need to provid a repository 'name'"
+}
+Repository repo = repository.repositoryManager.get(arg.name)
+
+if (repo != null) {
+    try {
+        repository.repositoryManager.delete(repo.name)
+        repo.destroy()
+    } catch(Exception e) {
+        return e.toString()
+    }
+    return true
+}
