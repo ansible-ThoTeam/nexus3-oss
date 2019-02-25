04f2fa88f210b403ac27baea45f9186646535d1a
Olivier Clavel <olivier.clavel@gmail.com>
2019-02-25 15:29:27 +0100

    Add json feedback to create_repos_from_list
    All future script must return feedback. We will enhance change detection once all scripts are done

--- files/groovy/create_repos_from_list.groovy
+++ files/groovy/create_repos_from_list.groovy
@@ -1,112 +1,132 @@
+import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import org.sonatype.nexus.repository.config.Configuration
 
 parsed_args = new JsonSlurper().parseText(args)
 
+List<Map<String, String>> actionDetails = []
+Map scriptResults = [changed: false, error: false]
+scriptResults.put('action_details', actionDetails)
+
 repositoryManager = repository.repositoryManager
 
 parsed_args.each { currentRepo ->
+
+    Map<String, String> currentResult = [name: currentRepo.name, format: currentRepo.format, type: currentRepo.type]
 
     recipeName = currentRepo.format + '-' + currentRepo.type
 
     existingRepository = repositoryManager.get(currentRepo.name)
 
-
-    if (existingRepository == null) {
-        log.info('Creating configuration for new repo `' + currentRepo.name + ' (Format: ' + currentRepo.format + ', Type: ' + currentRepo.type + ')')
-        // Default and/or immutable values
-        configuration = new Configuration(
-                repositoryName: currentRepo.name,
-                recipeName: recipeName,
-                online: true,
-                attributes: [
-                    storage: [
-                        blobStoreName: currentRepo.blob_store
+    try {
+        if (existingRepository == null) {
+            log.info('Creating configuration for new repo {} (Format: {},  Type: {})', currentRepo.name, currentRepo.format, currentRepo.type)
+            // Default and/or immutable values
+            configuration = new Configuration(
+                    repositoryName: currentRepo.name,
+                    recipeName: recipeName,
+                    online: true,
+                    attributes: [
+                            storage: [
+                                    blobStoreName: currentRepo.blob_store
+                            ]
                     ]
-                ]
-        )
-    } else {
-        log.info('Loading configuration for existing repo `' + currentRepo.name + ' (Format: ' + currentRepo.format + ', Type: ' + currentRepo.type)
-        // load existing repository configuration
-        configuration = existingRepository.configuration.copy()
+            )
+        } else {
+            log.info('Loading configuration for existing repo {} (Format: {},  Type: {})', currentRepo.name, currentRepo.format, currentRepo.type)
+            // load existing repository configuration
+            configuration = existingRepository.configuration.copy()
+        }
+
+        // Configs common to all repos
+        configuration.attributes['storage']['strictContentTypeValidation'] = Boolean.valueOf(currentRepo.strict_content_validation)
+
+        // Configs for all group repos
+        if (currentRepo.type == 'group') {
+            configuration.attributes['group'] = [
+                    memberNames: currentRepo.member_repos
+            ]
+        }
+
+        // Configs for all hosted repos
+        if (currentRepo.type == 'hosted') {
+            configuration.attributes['storage']['writePolicy'] = currentRepo.write_policy.toUpperCase()
+        }
+
+        // Configs for yum hosted repos
+        if (currentRepo.type == 'hosted' && currentRepo.format == 'yum') {
+            configuration.attributes['yum'] = [
+                    repodataDepth: currentRepo.repodata_depth.toInteger(),
+                    layoutPolicy : currentRepo.layout_policy.toUpperCase()
+            ]
+        }
+
+        // Configs for all proxy repos
+        if (currentRepo.type == 'proxy') {
+            authentication = currentRepo.remote_username == null ? null : [
+                    type    : 'username',
+                    username: currentRepo.remote_username,
+                    password: currentRepo.remote_password
+            ]
+
+            configuration.attributes['httpclient'] = [
+                    authentication: authentication,
+                    blocked       : false,
+                    autoBlock     : true,
+                    connection    : [
+                            useTrustStore: false
+                    ]
+            ]
+
+            configuration.attributes['proxy'] = [
+                    remoteUrl     : currentRepo.remote_url,
+                    contentMaxAge : currentRepo.get('maximum_component_age', 1440.0),
+                    metadataMaxAge: currentRepo.get('maximum_metadata_age', 1440.0)
+            ]
+        }
+
+        // Configs for docker proxy repos
+        if (currentRepo.type == 'proxy' && currentRepo.format == 'docker') {
+            configuration.attributes['dockerProxy'] = [
+                    indexType                  : currentRepo.index_type,
+                    useTrustStoreForIndexAccess: currentRepo.use_nexus_certificates_to_access_index
+            ]
+        }
+
+        // Configs for maven hosted/proxy repos
+        if (currentRepo.type in ['hosted', 'proxy'] && currentRepo.format == 'maven2') {
+            configuration.attributes['maven'] = [
+                    versionPolicy: currentRepo.version_policy.toUpperCase(),
+                    layoutPolicy : currentRepo.layout_policy.toUpperCase()
+            ]
+        }
+
+        // Configs for all docker repos
+        if (currentRepo.format == 'docker') {
+            configuration.attributes['docker'] = [
+                    forceBasicAuth: currentRepo.force_basic_auth,
+                    v1Enabled     : currentRepo.v1_enabled,
+                    httpPort      : currentRepo.get('http_port', '')
+            ]
+        }
+
+        if (existingRepository == null) {
+            repositoryManager.create(configuration)
+            currentResult.put('status', 'created')
+        } else {
+            repositoryManager.update(configuration)
+            currentResult.put('status', 'updated')
+        }
+        log.info('Configuration for repo {} saved', currentRepo.name)
+        scriptResults['changed'] = true
+
+    } catch (Exception e) {
+        currentResult.put('status', 'error')
+        currentResult.put('error_msg', e.toString())
+        scriptResults['error'] = true
+        log.error('Configuration for repo {} could not be saved: {}', currentRepo.name, e.toString())
     }
-
-    // Configs common to all repos
-    configuration.attributes['storage']['strictContentTypeValidation'] = Boolean.valueOf(currentRepo.strict_content_validation)
-
-    // Configs for all group repos
-    if (currentRepo.type == 'group') {
-        configuration.attributes['group'] = [
-                memberNames: currentRepo.member_repos
-        ]
-    }
-
-    // Configs for all hosted repos
-    if (currentRepo.type == 'hosted') {
-        configuration.attributes['storage']['writePolicy'] = currentRepo.write_policy.toUpperCase()
-    }
-
-    // Configs for yum hosted repos
-    if (currentRepo.type == 'hosted' && currentRepo.format == 'yum') {
-        configuration.attributes['yum'] = [
-                repodataDepth: currentRepo.repodata_depth.toInteger(),
-                layoutPolicy : currentRepo.layout_policy.toUpperCase()
-        ]
-    }
-
-    // Configs for all proxy repos
-    if (currentRepo.type == 'proxy') {
-        authentication = currentRepo.remote_username == null ? null : [
-                type    : 'username',
-                username: currentRepo.remote_username,
-                password: currentRepo.remote_password
-        ]
-
-        configuration.attributes['httpclient'] = [
-                authentication: authentication,
-                blocked       : false,
-                autoBlock     : true,
-                connection    : [
-                        useTrustStore: false
-                ]
-        ]
-
-        configuration.attributes['proxy'] = [
-                remoteUrl     : currentRepo.remote_url,
-                contentMaxAge : currentRepo.get('maximum_component_age', 1440.0),
-                metadataMaxAge: currentRepo.get('maximum_metadata_age', 1440.0)
-        ]
-    }
-
-    // Configs for docker proxy repos
-    if (currentRepo.type == 'proxy' && currentRepo.format == 'docker') {
-        configuration.attributes['dockerProxy'] = [
-                indexType                  : currentRepo.index_type,
-                useTrustStoreForIndexAccess: currentRepo.use_nexus_certificates_to_access_index
-        ]
-    }
-
-    // Configs for maven hosted/proxy repos
-    if (currentRepo.type in ['hosted', 'proxy'] && currentRepo.format == 'maven2') {
-        configuration.attributes['maven'] = [
-                versionPolicy: currentRepo.version_policy.toUpperCase(),
-                layoutPolicy : currentRepo.layout_policy.toUpperCase()
-        ]
-    }
-
-    // Configs for all docker repos
-    if (currentRepo.format == 'docker') {
-        configuration.attributes['docker'] = [
-                forceBasicAuth: currentRepo.force_basic_auth,
-                v1Enabled     : currentRepo.v1_enabled,
-                httpPort      : currentRepo.get('http_port', '')
-        ]
-    }
-
-    if (existingRepository == null) {
-        repositoryManager.create(configuration)
-    } else {
-        repositoryManager.update(configuration)
-    }
-    log.info('Configuration for repo `' + currentRepo.name + ' saved')
+    scriptResults['action_details'].add(currentResult)
 }
+
+return JsonOutput.toJson(scriptResults)
