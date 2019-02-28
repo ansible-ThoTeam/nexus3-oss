859e2252844ee54d7629df4771b38fc29f49f815
Olivier Clavel <olivier.clavel@gmail.com>
2019-02-28 22:30:10 +0100

    Fix ldap user groovy script

--- files/groovy/setup_ldap_users_from_list.groovy
+++ files/groovy/setup_ldap_users_from_list.groovy
@@ -13,42 +13,44 @@ scriptResults.put('action_details', actionDetails)
 
 parsed_args.each { currentUserDef ->
 
-	state = currentUserDef.get('state', 'present')
-	Map<String, String> currentResult = [username: currentUserDef.username, state: state, roles: currentUserDef.roles.join(', ')]
+    state = currentUserDef.get('state', 'present')
+    Map<String, String> currentResult = [name: currentUserDef.name, state: state, roles: currentUserDef.roles.join(', ')]
 
-	try {
-		User user = security.securitySystem.getUser(userDefinition.username, "LDAP")
-		authManager = security.getSecuritySystem().getAuthorizationManager(UserManager.DEFAULT_SOURCE)
-		currentUserDef.roles.each { role ->
-			log.info("RoleID : " + authManager.getRole(role).roleId.toString())
-			RoleIdentifier newRole = new RoleIdentifier("default", authManager.getRole(role).roleId);
-			if (state == 'absent') {
-				user.removeRole(newRole)
-				msg = "Role {} removed from user {}"
-				currentResult.put('status', 'removed')
-				scriptResults['changed'] = true
-			} else {
-				user.addRole(newRole)
-				msg = "Role {} added to user {}"
-				currentResult.put('status', 'created')
-				scriptResults['changed'] = true
-			}
-			security.securitySystem.setUsersRoles(user.getUserId(), "LDAP", user.getRoles());
-			log.info(msg, authManager.getRole(role).roleId.toString(), user.getUserId() )
-		}
-	} catch(UserNotFoundException e) {
-		if (state == 'absent') {
-			currentResult.put('status', 'absent')
-		} else {
-			currentResult.put('status', 'error')
-			currentResult.put('error_msg', "User could not be found")
-			scriptResults['error'] = true
-		}
-	} catch (Exception e) {
-		currentResult.put('status', 'error')
-		currentResult.put('error_msg', e.toString())
-	}
-	scriptResults['action_details'].add(currentResult)
+    try {
+        User user = security.getSecuritySystem().getUser(currentUserDef.name,"LDAP")
+        authManager = security.getSecuritySystem().getAuthorizationManager(UserManager.DEFAULT_SOURCE)
+        currentResult.put('status', 'roles unchanged')
+        currentUserDef.roles.each { role ->
+            log.info("RoleID : " + authManager.getRole(role).roleId.toString())
+            Set<RoleIdentifier> existingRoles = user.getRoles()
+            RoleIdentifier newRole = new RoleIdentifier("default", authManager.getRole(role).roleId);
+            if (state == 'absent' && newRole in existingRoles) {
+                user.removeRole(newRole)
+                log.info("Role {} removed from user {}", authManager.getRole(role).roleId.toString(), user.getUserId() )
+                currentResult.put('status', 'roles removed')
+                scriptResults['changed'] = true
+            } else if (state == 'present' && !(newRole in existingRoles)) {
+                user.addRole(newRole)
+                log.info("Role {} added to user {}", authManager.getRole(role).roleId.toString(), user.getUserId() )
+                currentResult.put('status', 'roles updated')
+                scriptResults['changed'] = true
+            }
+        }
+        security.securitySystem.setUsersRoles(user.getUserId(), "LDAP", user.getRoles());
+    } catch(UserNotFoundException e) {
+        if (state == 'absent') {
+            currentResult.put('status', 'absent')
+        } else {
+            currentResult.put('status', 'error')
+            currentResult.put('error_msg', "User could not be found")
+            scriptResults['error'] = true
+        }
+    } catch (Exception e) {
+        currentResult.put('status', 'error')
+        currentResult.put('error_msg', e.toString())
+        scriptResults['error'] = true
+    }
+    scriptResults['action_details'].add(currentResult)
 }
 
 return JsonOutput.toJson(scriptResults)
