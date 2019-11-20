c1c60450d6c33f4583fa854e796cda78e44122c9
Olivier Clavel <olivier.clavel@gmail.com>
2019-11-20 13:11:03 +0100

    Extend task creation possibilities
    - Allow to set start date (and differ now/once)
    - Add weekly and monthly schedules
    - Add tests for all schedules

--- files/groovy/create_task.groovy
+++ files/groovy/create_task.groovy
@@ -2,7 +2,10 @@ import groovy.json.JsonSlurper
 import org.sonatype.nexus.scheduling.TaskConfiguration
 import org.sonatype.nexus.scheduling.TaskInfo
 import org.sonatype.nexus.scheduling.TaskScheduler
+import org.sonatype.nexus.scheduling.schedule.Monthly
 import org.sonatype.nexus.scheduling.schedule.Schedule
+import org.sonatype.nexus.scheduling.schedule.Weekly
+import java.text.SimpleDateFormat
 
 parsed_args = new JsonSlurper().parseText(args)
 
@@ -35,9 +38,34 @@ parsed_args.booleanTaskProperties.each { key, value ->
     taskConfiguration.setBoolean(key, Boolean.valueOf(value))
 }
 
-Schedule schedule = null;
+// Init empty/default vars from script call
 
-switch( parsed_args.schedule_type ) {
+// Type of schedule. Defaults to cron
+type = parsed_args.get('schedule_type', 'cron')
+
+// Cron expression. Used only for cron schecule. Defaults to null
+cron = parsed_args.get('cron', null)
+
+// Start date time. Defaults to now. Unused for manual, now and cron schedules
+// This is our expected date format
+SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+date_time_string = parsed_args.get('start_date_time', null)
+Date start_date
+if (date_time_string)
+    start_date = dateFormat.parse(date_time_string)
+else
+    start_date = new Date()
+
+// List of weekdays to run task. Used for weekly schedule. Need at least one
+// defaults to null which will error if this schedule is chosen
+weekly_days = parsed_args.get('weekly_days', null)
+
+// List of calendar days to run task. Used for monthly schedule. Need at leas one
+// defaults to null which will error if this schedule is chosen
+monthly_days = parsed_args.get('monthly_days', null)
+
+Schedule schedule
+switch(type) {
     case 'manual':
         schedule = taskScheduler.scheduleFactory.manual()
         break
@@ -45,16 +73,38 @@ switch( parsed_args.schedule_type ) {
         schedule = taskScheduler.scheduleFactory.now()
         break
     case 'once':
-        schedule = taskScheduler.scheduleFactory.once(new Date())
+        schedule = taskScheduler.scheduleFactory.once(start_date)
         break
     case 'hourly':
-        schedule = taskScheduler.scheduleFactory.hourly(new Date())
+        schedule = taskScheduler.scheduleFactory.hourly(start_date)
         break
     case 'daily':
-        schedule = taskScheduler.scheduleFactory.daily(new Date())
+        schedule = taskScheduler.scheduleFactory.daily(start_date)
+        break
+    case 'weekly':
+        if (!weekly_days)
+            throw new Exception('Weekly scehedule requires a weekly_days list parameter')
+        Set<Weekly.Weekday> weekdays = []
+        weekly_days.each { day ->
+            weekdays.add(Weekly.Weekday.valueOf(day))
+        }
+        schedule = taskScheduler.scheduleFactory.weekly(start_date, weekdays)
+        break
+    case 'monthly':
+        if (!monthly_days)
+            throw new Exception('Monthly scehedule requires a weekly_days list parameter')
+        Set<Monthly.CalendarDay> calendardays = []
+        monthly_days.each { day ->
+            calendardays.add(Monthly.CalendarDay.day(day as Integer))
+        }
+        schedule = taskScheduler.scheduleFactory.monthly(start_date, calendardays)
+        break
+    case 'cron':
+        schedule = taskScheduler.scheduleFactory.cron(new Date(), cron)
         break
     default:
-        schedule = taskScheduler.scheduleFactory.cron(new Date(), parsed_args.cron)
+        /** @todo: dont crash, return error in json response **/
+        throw new Exception("Unknown schedule type: " + parsed_args.schedule_type.toString())
         break
 }
 
