76bb4e88eff748ae22154f1b502f64de6972f868
Serienmorder <pyro985@gmail.com>
2020-04-29 09:27:46 -0700

    Add tests, replace delete default blobstore section.

--- files/groovy/delete_blobstore.groovy
+++
@@ -1,8 +0,0 @@
-import groovy.json.JsonSlurper
-
-parsed_args = new JsonSlurper().parseText(args)
-
-existingBlobStore = blobStore.getBlobStoreManager().get(parsed_args.name)
-if (existingBlobStore != null) {
-    blobStore.getBlobStoreManager().delete(parsed_args.name)
-}
